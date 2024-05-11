fa80652e8bdc199c740f82772d44a3ff57a41658
L00tz1ffer <88904868+L00tz1ffer@users.noreply.github.com>
2024-05-11 18:03:52 +0200

    initial commit

---
+++ Algebra.js
@@ -0,0 +1,112 @@
+const logName = "Algebra: ";
+
+class Algebra {
+  constructor() {}
+
+  isValidInt(number) {
+    return number >= 0 && number <= 9;
+  }
+
+  convertNumbersArrayToString(number) {
+    let buildVal = "";
+    number.forEach((element) => {
+      buildVal = buildVal + element;
+    });
+    return buildVal;
+  }
+
+  convertNumberStringToArray(number, delimiter) {
+    // console.log("Converting...");
+    const numberAsArray = [];
+
+    for (let i = 0; i < number.length; i++) {
+      if (this.isValidInt(number[i])) {
+        numberAsArray.push(number[i]);
+        // console.log(number[i]);
+      }
+      if (delimiter == number[i]) {
+        numberAsArray.push(number[i]);
+        // console.log(number[i]);
+      }
+      if ("-" === number[i] && i === 0) {
+        numberAsArray.push(number[i]);
+        // console.log(number[i]);
+      }
+    }
+
+    return numberAsArray;
+  }
+
+  roundArray(number, delimiter) {
+    // console.log("trying to round...");
+    // Überprüfen, ob das Array num1 den delimiter enthält
+    const index = number.indexOf(delimiter);
+
+    if (index !== -1) {
+      // Abschneiden des Arrays bis zum delimiter und umkehren
+      const n1 = number.slice(0, index);
+      return n1;
+    } else {
+      // Wenn der delimiter nicht gefunden wurde, einfach das originale Array zurückgeben
+      return number;
+    }
+  }
+
+  removeLeadingNumbersFromString(number) {
+    return number.replace(/^0+/, "");
+  }
+
+  getNumber(number) {
+    number = parseInt(number);
+    if (
+      Number.isNaN(number) ||
+      !Number.isFinite(number) ||
+      number === null ||
+      number === undefined
+    ) {
+      return 0;
+    } else {
+      return number;
+    }
+  }
+
+  addArrays(num1, num2, delimiter) {
+    let result;
+    let n1 = this.roundArray(num1, delimiter).reverse();
+    let n2 = this.roundArray(num2, delimiter).reverse();
+
+    // console.log("Starting to Add..");
+    // console.log(n1);
+    // console.log(n2);
+
+    let resLen = n1.length + n2.length;
+
+    let res = Array(resLen + 1);
+
+    let rest = 0;
+
+    for (let index = 0; index < resLen; index++) {
+      let sum = +this.getNumber(n1[index]) + this.getNumber(n2[index]);
+
+      sum = sum + this.getNumber(rest);
+
+      rest = 0;
+      if (sum >= 10) {
+        rest = 1;
+        result = 0;
+      } else {
+        result = sum;
+      }
+      // console.log("Round: " + index + ", Result: " + result + " Rest: " + rest);
+      res.push(result);
+    }
+
+    return res.reverse();
+  }
+
+  subtractArrays () {
+    
+  }
+}
+
+export default Algebra;
---
+++ index.js
@@ -0,0 +1,28 @@
+import Algebra from "./Algebra.js";
+
+const A = new Algebra();
+const delimiter = "."
+// const startVal = "12"
+
+// let value = A.convertNumberStringToArray(startVal, delimiter)
+// console.log(value)
+
+// console.log(A.addArrays(value, value, delimiter))
+
+// console.log(A.removeLeadingNumbersFromString(A.convertNumbersArrayToString(A.addArrays(value, value, delimiter))))
+// console.log(A.removeLeadingNumbersFromString(A.convertNumbersArrayToString(value)))
+
+let numbers = {
+    1: "1",
+    2: "1",
+    3: "1"
+}
+
+while (true){
+    
+    numbers[3] = A.addArrays(A.convertNumberStringToArray(numbers[1],delimiter),A.convertNumberStringToArray(numbers[2],delimiter));
+    numbers[2] = numbers[1];
+    numbers[1] = numbers[3];
+    console.log( "Fibonacci: " + A.removeLeadingNumbersFromString(A.convertNumbersArrayToString(numbers[3] )))
+}
+
